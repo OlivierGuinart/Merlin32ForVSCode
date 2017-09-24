6ef863618e18caaf410c136f9e55ad25ff13d8b1
Olivier Guinart <olivier_guinart+github@live.com>
2017-09-23 18:04:55 -0700

    hover functionality initial move to its own module

---
+++ src/hover.ts
@@ -0,0 +1,48 @@
+import * as vscode from 'vscode';
+import * as data from '../strings/resources.json';
+
+// create an empty decorator type (no need for border around the opcode nor different background color)
+// this is just to enable hover.
+const opcodesDecorationType = vscode.window.createTextEditorDecorationType({ });
+
+var _regExp: RegExp;
+export function initializeOpcodesRegex() {
+    let r : string = "\\b(";
+    for (var prop in data) {
+        if (prop != "--^") {
+            r += prop + "|";
+        }
+    }
+    // The last '|(--\\^)' group below is the ELUP synomym,
+    // not considered a word per regex standards, so not in \b boundaries group.
+    _regExp = new RegExp(r.substr(0, r.length - 1) + ")\\b|(--\\^)", 'gi'); 
+}
+
+let _activeEditor: vscode.TextEditor;
+var _timeout = null;
+export function triggerUpdateDecorations(activeEditor: vscode.TextEditor) : NodeJS.Timer {
+    _activeEditor = activeEditor;
+    if (_timeout) {
+        clearTimeout(_timeout);
+    }
+    _timeout = setTimeout(updateDecorations, 500);
+    return _timeout;
+}
+
+function updateDecorations() {
+    if (!_activeEditor) {
+        return;
+    }
+    const text = _activeEditor.document.getText();
+    const opcodes: vscode.DecorationOptions[] = [];
+    let match;
+    while (match = _regExp.exec(text)) {
+        const startPos = _activeEditor.document.positionAt(match.index);
+        const endPos = _activeEditor.document.positionAt(match.index + match[0].length);
+        const decoration = { range: new vscode.Range(startPos, endPos), hoverMessage: data[match[0].toUpperCase()] };
+        opcodes.push(decoration);
+    }
+    if (opcodes.length > 0) {
+        _activeEditor.setDecorations(opcodesDecorationType, opcodes);
+    }
+}
